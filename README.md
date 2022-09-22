ce8117f530e23ae6c9479e83d6a90cc39d0c9550
thordossche <dossche.thor@gmail.com>
2022-09-22 10:53:08 +0200

    cleanup

--- README.md
+++ README.md
@@ -1,1 +1,8 @@
 # openseekmap
+
+# compile
+```
+cd src
+gcc main.c options_parser.c search.c haversine.c entry.c match.c ranking.c datamap.c linkedlist.c query.c edit_distance.c shift_and.c ../assignment/accents.c ../assignment/utf8.c -g -o ./openseekmap -Wall -Werror -lm -std=c18
+echo [query] | ./openseekmap [database]
+```
--- src/run.sh
+++
@@ -1,5 +0,0 @@
-# gcc main.c -g -o openseekmap -Wall -Werror -std=c18 && echo 'query' | valgrind --trace-children=yes --track-fds=yes --track-origins=yes --leak-check=full --show-leak-kinds=all ./openseekmap ../belgie.data
-
-gcc main.c options_parser.c search.c haversine.c entry.c match.c ranking.c datamap.c linkedlist.c query.c edit_distance.c shift_and.c ../assignment/accents.c ../assignment/utf8.c -g -o openseekmap -Wall -Werror -lm -std=c18 && cat "$1" | valgrind -s --leak-check=full --show-leak-kinds=all ./openseekmap ../belgie.data
-
-#gcc main.c options_parser.c search.c entry.c match.c linkedlist.c edit_distance.c shift_and.c -O3 -o openseekmap -Wall -Werror -std=c18 && cat "$1" | ./openseekmap ../belgie.data
